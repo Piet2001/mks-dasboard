91bf33952dcf5a704431f0abd50a39f779078204
Piet2001 <piet2001@fireman.net>
2021-08-04 11:04:00 +0200

    remove learn react

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Momenteel werken wij aan een nieuwe versie voor het mks-dashboard. Deze pagina is daarom voorlopig nog niet beschikbaar.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
