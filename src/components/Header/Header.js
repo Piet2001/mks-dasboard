0c92539834867178c3bcd205a87e6bb3712596a3
Piet2001 <piet2001@fireman.net>
2021-08-04 12:56:02 +0200

    fix warning

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,12 @@ import { Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { RiPoliceCarFill } from "react-icons/ri";
 import { FaBuilding, FaHome } from "react-icons/fa";
-import logo from "../../mkslogo.png"
 import React from "react";
 
 const Header = () => {
   return (
     <div className="header-wrapper">
       <Navbar className="navbar" expand="lg">
-        {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
             <Link className="link" to="/mks-dashboard">
